44ed32a2cab65b77ddddeb8797954cadb64eb4ca
badermah <bmahdawi1@student.rccd.edu>
2023-04-04 15:06:28 -0700

    First Commit

---
+++ Develop/index.js
@@ -0,0 +1,24 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+// TODO: Create an array of questions for user input
+const questions = [ {
+    type: 'input',
+    name: 'title',
+    message: 'Enter your project title:',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Enter a description for your project:',
+  },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
